7f20292a3115a763c37c1ca8d970bfbff9388769
erikka-22 <iwasaki.pingoo@gmail.com>
2024-02-04 21:30:15 +0900

    画面キャプチャのダウンロードを実装

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,51 @@
 "use client"
 
-import VideoArea from "@/app/components/VideoArea"
-import ShotButton from "@/app/components/ShotButton"
-import BackgrountImageList from "@/app/components/BackgroundImageList"
+import { useRef, useCallback, useState } from "react"
+import Webcam from "react-webcam"
+
+const videoConstraints = {
+  width: 720,
+  height: 360,
+  facingMode: "user"
+}
+
+// import VideoArea from "@/app/components/VideoArea"
+// import ShotButton from "@/app/components/ShotButton"
+// import BackgrountImageList from "@/app/components/BackgroundImageList"
 
 export default function Page() {
+  const webcamRef = useRef<Webcam>(null)
+  const [url, setUrl] = useState<string | null>(null)
+  const handleDownload = (url: string) => {
+    const link = document.createElement('a')
+    link.href = url
+    link.download = 'capture.png'
+    document.body.appendChild(link)
+    link.click()
+    document.body.removeChild(link)
+    URL.revokeObjectURL(url)
+  }
+  const capture = useCallback(() => {
+    const imageSrc = webcamRef.current?.getScreenshot()
+    if (imageSrc) {
+      setUrl(imageSrc)
+      handleDownload(imageSrc)
+    }
+  }, [webcamRef])
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <VideoArea />
-      <ShotButton />
-      <BackgrountImageList />
+      <div>
+        <Webcam 
+          audio={false}
+          width={540}
+          height={360}
+          ref={webcamRef}
+          screenshotFormat="image/jpeg"
+          videoConstraints={videoConstraints}
+        />
+      </div>
+      <button onClick={capture}>撮影する</button>
+      <p>{url}</p>
     </main>
   );
 }
