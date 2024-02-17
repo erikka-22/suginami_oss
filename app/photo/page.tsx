bebbd17a308b8ae83cd705aee836bdc43b871df1
erikka-22 <iwasaki.pingoo@gmail.com>
2024-02-17 20:54:51 +0900

    localStorageの操作をgetterでラップ

--- app/photo/page.tsx
+++ app/photo/page.tsx
@@ -1,12 +1,17 @@
 "use client"
 
 import Image from "next/image";
+import { useState, useEffect } from "react"
 
 export default function Page() {
-  let photoUrl = localStorage.getItem("photoUrl")
-  if (photoUrl === null) {
-    photoUrl = ""
-  }
+  const [photoUrl, setPhotoUrl] = useState('')
+  
+  const getPhotoUrl = () => localStorage.getItem("photoUrl") || ""
+
+  useEffect(() => {
+    setPhotoUrl(getPhotoUrl())
+  }, [])
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <Image 
