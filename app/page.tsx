eb0148725ce4c3dec9c5405907b53a14062f692c
tanimuranaomichi <tanimuranaomichi@gmail.com>
2024-02-20 22:54:58 +0900

    Add CameraPreviewStream and update dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -1,51 +1,41 @@
-"use client"
+"use client";
 
-import { useRef, useCallback, useState } from "react"
-import Webcam from "react-webcam"
+import { useRef, useCallback, useState } from "react";
+import Webcam from "react-webcam";
+import CameraPreviewStream from "./components/CameraPreviewStream";
 
 const videoConstraints = {
   width: 720,
   height: 360,
-  facingMode: "user"
-}
+  facingMode: "user",
+};
 
 // import VideoArea from "@/app/components/VideoArea"
 // import ShotButton from "@/app/components/ShotButton"
 // import BackgrountImageList from "@/app/components/BackgroundImageList"
 
 export default function Page() {
-  const webcamRef = useRef<Webcam>(null)
-  const [url, setUrl] = useState<string | null>(null)
+  const webcamRef = useRef<Webcam>(null);
+  const [url, setUrl] = useState<string | null>(null);
   const handleDownload = (url: string) => {
-    const link = document.createElement('a')
-    link.href = url
-    link.download = 'capture.png'
-    document.body.appendChild(link)
-    link.click()
-    document.body.removeChild(link)
-    URL.revokeObjectURL(url)
-  }
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = "capture.png";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+    URL.revokeObjectURL(url);
+  };
   const capture = useCallback(() => {
-    const imageSrc = webcamRef.current?.getScreenshot()
+    const imageSrc = webcamRef.current?.getScreenshot();
     if (imageSrc) {
-      setUrl(imageSrc)
-      handleDownload(imageSrc)
+      setUrl(imageSrc);
+      handleDownload(imageSrc);
     }
-  }, [webcamRef])
+  }, [webcamRef]);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div>
-        <Webcam 
-          audio={false}
-          width={540}
-          height={360}
-          ref={webcamRef}
-          screenshotFormat="image/jpeg"
-          videoConstraints={videoConstraints}
-        />
-      </div>
-      <button onClick={capture}>撮影する</button>
-      <p>{url}</p>
+      <CameraPreviewStream />
     </main>
   );
 }
